f13c1d58f30339a9844c970d892f77cca2828f1a
Manuher121 PC <manuher1229@hotmail.com>
2025-06-17 00:54:39 -0400

    Added some more info and modules

--- app.py
+++ app.py
@@ -1,13 +1,37 @@
-from flask import Flask
+from flask import Flask, request, session
 from flask import render_template
+from helpers import login_required
+from flask_session import Session
 
 app = Flask(__name__)   
 
-@app.route('/') 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route('/')
+@login_required 
 def index():
     return render_template('layout.html')
 
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    session.clear()
+
+
+
+
+
+
+
+
+
+
+
+
+#@app.route("/register", method=["GET", "POST"])
+#def register():
+    #register user
