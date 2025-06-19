db3c036c5f08c096c4bbf33f96713b4e598bb41d
Manuher121 PC <manuher1229@hotmail.com>
2025-06-19 02:57:27 -0400

    Login works, register works.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask, request, session, render_template, redirect
 from helpers import login_required, apology
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import select, create_engine
-import os
+from sqlalchemy import select, create_engine, insert
+from werkzeug.security import check_password_hash, generate_password_hash
+
 
 app = Flask(__name__)   
 
@@ -26,44 +28,87 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route('/')
 @login_required 
 def index():
     return render_template('layout.html')
 
 
-
 @app.route("/login", methods=["GET", "POST"])
 def login():
 
-
+    session.clear()
     
     if request.method == "POST":
         if not request.form.get("username"):
             return apology("must give a username", 403)
         elif not request.form.get("password"):
             return apology("must provide password", 403)
-        
-
-
+                
         current_user = select(User).where(User.username == (request.form.get("username")))
+
         with engine.connect() as conn:
             result = conn.execute(current_user)
             for row in result:
                 new_result = row
-
-        #if current_user.hash == request.form.get("password"):
-        #   session["user_id"] =  current_user.id
-        #else:
-        #    return apology("incorrect password", 403)
-
-        return render_template("login.html", user = new_result.hash)
-
-
+        try:
+            if check_password_hash(new_result.hash, request.form.get("password")):
+                session["user_id"] =  new_result.id
+                return redirect("/")
+            else:
+                return apology("incorrect password", 403)
+        except: 
+            return apology("incorrect username", 403)
     else:
         return render_template("login.html")
 
-
-#@app.route("/register", method=["GET", "POST"])
-#def register():
+@app.route("/register", methods=["GET", "POST"])
+def register():
     #register user
+
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("must provide a username", 400)
+        if not request.form.get("email"):
+            return apology("must provide a email", 400)
+        if not request.form.get("password"):
+            return apology("must provide a password", 400)
+        if request.form.get("confirmation") != request.form.get("password"):
+            return apology("passwords must match", 400)
+
+        try:
+            current_user = insert(User).values(username = (request.form.get("username")), hash = (generate_password_hash(request.form.get("password"), 
+                                            method='scrypt', salt_length=16)), email = (request.form.get("email")))
+
+            with engine.connect() as conn:
+                result = conn.execute(current_user)
+                conn.commit()
+        except:
+            return apology("user already in use", 400)
+
+        return redirect("/")
+    else:
+        return render_template("register.html")
+
+
+
+
+
+
+
+
+
+@app.route("/logout")
+def logout():
+
+    session.clear()
+
+    return redirect("/")
